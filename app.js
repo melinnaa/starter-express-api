a44e645cedac28be1ca175ec7b2c0b0ec7187d19
Melinna Agudelo <melinna@Winnie.lan>
2022-12-04 00:49:46 +0100

    yoyo

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-
+    res.send("hola");
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
